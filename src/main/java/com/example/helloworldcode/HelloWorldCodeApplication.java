c151486d9d63c39022c3624fdc98747d04f60c9f
wbruss <kihoropaul@gmail.com>
2023-03-11 11:37:50 +0300

    Project initialization

---
+++ src/main/java/com/example/helloworldcode/HelloWorldCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/helloworldcode/HelloWorldCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
